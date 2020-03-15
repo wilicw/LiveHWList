dcb4600e5f6b4b0680a2fb1161bbe8ea81309859
William Chang <wilicw1346@gmail.com>
2020-03-15 22:54:30 +0800

    add notification

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js')
+importScripts('https://cdn.jsdelivr.net/npm/localforage@1.7.3/dist/localforage.min.js')
 
 workbox.precaching.precacheAndRoute([
     {url:'/index.html', revision: 1}
@@ -24,9 +25,32 @@ workbox.routing.registerRoute(
     })
 )
 
+let notification_alert_check = () => {
+    return new Promise((resolve, reject) => {
+        setInterval(() => {
+            localforage.getItem('notification').then(value => {
+                if (value) {
+                    let reload = value
+                    value.map(item => {
+                        let now = new Date().getTime()
+                        console.log("now", now)
+                        console.log("Task", item.time)
+                        if (item.time <= now) {
+                            self.registration.showNotification(item.title)
+                            reload = reload.filter(i => i.id !== item.id)
+                        }
+                    })
+                    localforage.setItem('notification', reload)
+                }
+            })
+        }, 10*1000)
+        // reject(new Error("not yet"))
+    })
+}
+
 self.addEventListener('sync', function (e) {
     console.log(`service worker tag: ${e.tag}`)
     if (e.tag === 'notification_sync') {
-        e.waitUntil()
+        e.waitUntil(notification_alert_check())
     }
 })
